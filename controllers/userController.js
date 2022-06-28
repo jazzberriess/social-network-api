c8e9c63f987ffa855356052091239f38014d0991
Jazzberries <anime.flick.chick@gmail.com>
2022-06-28 11:28:02 +1000

    Added getsingleUser route and post new user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,41 @@
 const User = require('../models/User');
 
-getAllUsers = async (req, res) => {
+const getAllUsers = async (req, res) => {
   try {
     const allUsers = await User.find();
 
     if (!allUsers) {
       return res.status(404).json({ message: 'No users found' });
     } else {
-      res.json(allUsers);
+      res.status(200).json(allUsers);
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+};
+
+const getSingleUser = async (req, res) => {
+  try {
+    const singleUser = await User.findOne({ _id: req.params.userId }).select(
+      '-__v'
+    );
+
+    if (!singleUser) {
+      return res.status(404).json({ message: 'No user with that ID found' });
+    } else {
+      res.status(200).json(singleUser);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+};
+
+const createNewUser = async (req, res) => {
+  try {
+    const createUser = await User.create(req.body);
+    res.status(200).json({ message: 'New user added!', createUser });
   } catch (error) {
     console.error(error);
     res.status(500).json(error);
@@ -17,4 +44,6 @@ getAllUsers = async (req, res) => {
 
 module.exports = {
   getAllUsers,
+  getSingleUser,
+  createNewUser,
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
-const { getAllUsers } = require('../../controllers/userController');
+const {
+  getAllUsers,
+  getSingleUser,
+  createNewUser,
+} = require('../../controllers/userController');
 
-//route for /api/users
+//get route for /api/users
 router.route('/').get(getAllUsers);
+
+//post route for api/users
+router.route('/').post(createNewUser);
+
+//get route for single user: /api/users/:userId
+router.route('/:userId').get(getSingleUser);
 
 module.exports = router;
